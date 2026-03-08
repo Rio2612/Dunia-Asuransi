469638301402d59f2b7786533a6352840ad9cfa0
Rio2612 <141880694+Rio2612@users.noreply.github.com>
2026-03-08 18:47:46 +0700

    Update StructuredData.tsx

--- src/components/StructuredData.tsx
+++ src/components/StructuredData.tsx
@@ -1,49 +1,25 @@
 export default function StructuredData() {
-  const schema = {
-    "@context": "https://schema.org",
-    "@graph": [
-      {
-        "@type": "Organization",
-        "@id": "https://www.duniaasuransi.com/#organization",
-        name: "Dunia Asuransi",
-        url: "https://www.duniaasuransi.com",
-        logo: "https://www.duniaasuransi.com/logo.svg",
-        description:
-          "Layanan konsultasi asuransi profesional oleh Rio Mardiansyah untuk kendaraan, properti, kargo, dan kecelakaan.",
-      },
-
-      {
-        "@type": "Person",
-        "@id": "https://www.duniaasuransi.com/#person",
-        name: "Rio Mardiansyah",
-        jobTitle: "Konsultan Asuransi",
-        worksFor: {
-          "@id": "https://www.duniaasuransi.com/#organization",
-        },
-        url: "https://www.duniaasuransi.com",
-      },
-
-      {
-        "@type": "Service",
-        name: "Konsultasi Asuransi",
-        provider: {
-          "@id": "https://www.duniaasuransi.com/#organization",
-        },
-        areaServed: "Indonesia",
-        serviceType: [
-          "Asuransi Kendaraan",
-          "Asuransi Kebakaran",
-          "Asuransi Kargo",
-          "Asuransi Kecelakaan"
-        ]
-      }
-    ]
-  };
-
   return (
-    <script
-      type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
-    />
-  );
+    <>
+      <script type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
+      />
+
+      <script type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessSchema) }}
+      />
+
+      <script type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
+      />
+
+      <script type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(personSchema) }}
+      />
+
+      <script type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(aggregateRatingSchema) }}
+      />
+    </>
+  )
 }
