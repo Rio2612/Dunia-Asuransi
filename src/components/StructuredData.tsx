e006ca0d57e85016b808656985eedf78aea04a1d
Rio2612 <141880694+Rio2612@users.noreply.github.com>
2026-03-08 19:04:00 +0700

    Update StructuredData.tsx

--- src/components/StructuredData.tsx
+++ src/components/StructuredData.tsx
@@ -1,25 +1,122 @@
+const baseUrl = "https://www.duniaasuransi.com";
+
+const organizationSchema = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "Dunia Asuransi",
+  "url": baseUrl,
+  "logo": `${baseUrl}/logo.png`,
+  "contactPoint": {
+    "@type": "ContactPoint",
+    "telephone": "+628131556592",
+    "contactType": "customer service",
+    "areaServed": "ID",
+    "availableLanguage": ["Indonesian"]
+  },
+  "sameAs": []
+};
+
+const localBusinessSchema = {
+  "@context": "https://schema.org",
+  "@type": "InsuranceAgency",
+  "name": "Dunia Asuransi",
+  "image": `${baseUrl}/logo.png`,
+  "url": baseUrl,
+  "telephone": "+628131556592",
+  "address": {
+    "@type": "PostalAddress",
+    "addressLocality": "Batam",
+    "addressRegion": "Kepulauan Riau",
+    "addressCountry": "ID"
+  },
+  "areaServed": {
+    "@type": "Country",
+    "name": "Indonesia"
+  }
+};
+
+const websiteSchema = {
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "name": "Dunia Asuransi",
+  "url": baseUrl,
+  "potentialAction": {
+    "@type": "SearchAction",
+    "target": `${baseUrl}/search?q={search_term_string}`,
+    "query-input": "required name=search_term_string"
+  }
+};
+
+const personSchema = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  "name": "Rio Mardiansyah",
+  "jobTitle": "Insurance Consultant",
+  "worksFor": {
+    "@type": "Organization",
+    "name": "Dunia Asuransi"
+  },
+  "url": baseUrl
+};
+
+const serviceSchema = {
+  "@context": "https://schema.org",
+  "@type": "Service",
+  "name": "Konsultasi Asuransi Indonesia",
+  "provider": {
+    "@type": "Organization",
+    "name": "Dunia Asuransi",
+    "url": baseUrl
+  },
+  "areaServed": {
+    "@type": "Country",
+    "name": "Indonesia"
+  }
+};
+
+const aggregateRatingSchema = {
+  "@context": "https://schema.org",
+  "@type": "Service",
+  "name": "Layanan Konsultasi Dunia Asuransi",
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "5",
+    "reviewCount": "6"
+  },
+  "provider": {
+    "@type": "Organization",
+    "name": "Dunia Asuransi",
+    "url": baseUrl
+  }
+};
+
 export default function StructuredData() {
   return (
     <>
-      <script type="application/ld+json"
+      <script
+        type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
       />
-
-      <script type="application/ld+json"
+      <script
+        type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessSchema) }}
       />
-
-      <script type="application/ld+json"
+      <script
+        type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
       />
-
-      <script type="application/ld+json"
+      <script
+        type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(personSchema) }}
       />
-
-      <script type="application/ld+json"
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(serviceSchema) }}
+      />
+      <script
+        type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(aggregateRatingSchema) }}
       />
     </>
-  )
+  );
 }
