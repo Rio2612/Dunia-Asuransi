c8cd131383f4c51cc1d6728523b99e826ecc40e1
Asuransi Kito <141880694+Rio2612@users.noreply.github.com>
2026-04-02 11:37:51 +0700

    Refactor StructuredData component and schemas

--- src/components/StructuredData.tsx
+++ src/components/StructuredData.tsx
@@ -1,85 +1,37 @@
-const baseUrl = "https://www.duniaasuransi.com";
-
-const organizationSchema = {
-  "@context": "https://schema.org",
-  "@type": "Organization",
-  "name": "Dunia Asuransi",
-  "url": baseUrl,
-  "logo": `${baseUrl}/logo.png`,
-  "contactPoint": {
-    "@type": "ContactPoint",
-    "telephone": "+628131556592",
-    "contactType": "customer service",
-    "areaServed": "ID",
-    "availableLanguage": ["Indonesian"]
-  },
-  "sameAs": []
-};
-
-const localBusinessSchema = {
-  "@context": "https://schema.org",
-  "@type": "InsuranceAgency",
-  "name": "Dunia Asuransi",
-  "image": `${baseUrl}/logo.png`,
-  "url": baseUrl,
-  "telephone": "+628131556592",
-  "address": {
-    "@type": "PostalAddress",
-    "addressLocality": "Batam",
-    "addressRegion": "Kepulauan Riau",
-    "addressCountry": "ID"
-  },
-  "areaServed": {
-    "@type": "Country",
-    "name": "Indonesia"
-  }
-};
-
-const websiteSchema = {
-  "@context": "https://schema.org",
-  "@type": "WebSite",
-  "name": "Dunia Asuransi",
-  "url": baseUrl,
-  "potentialAction": {
-    "@type": "SearchAction",
-    "target": `${baseUrl}/search?q={search_term_string}`,
-    "query-input": "required name=search_term_string"
-  }
-};
-
-const personSchema = {
-  "@context": "https://schema.org",
-  "@type": "Person",
-  "name": "Rio Mardiansyah",
-  "jobTitle": "Insurance Consultant",
-  "worksFor": {
-    "@type": "Organization",
-    "name": "Dunia Asuransi"
-  },
-  "url": baseUrl
-};
-
-// CATATAN:
-// Saya menghapus 'serviceSchema' di sini karena sudah ada di page.tsx (dengan detail yang lebih lengkap).
-// Jadi ini menghindari duplikat.
-
-const aggregateRatingSchema = {
-  "@context": "https://schema.org",
-  "@type": "Service",
-  "name": "Layanan Konsultasi Dunia Asuransi",
-  "aggregateRating": {
-    "@type": "AggregateRating",
-    "ratingValue": "5",
-    "reviewCount": "6"
-  },
-  "provider": {
-    "@type": "Organization",
-    "name": "Dunia Asuransi",
-    "url": baseUrl
-  }
-};
+'use client'
 
 export default function StructuredData() {
+  const organizationSchema = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "name": "Dunia Asuransi",
+    "url": "https://www.duniaasuransi.com",
+    "logo": "https://www.duniaasuransi.com/logo.png",
+    "description": "Dunia Asuransi - Broker asuransi terpercaya untuk asuransi kendaraan, alat berat, dan properti di Indonesia.",
+    "telephone": "+628131556592",
+    "email": "info@duniaasuransi.com",
+    "address": {
+      "@type": "PostalAddress",
+      "addressCountry": "ID",
+      "addressLocality": "Indonesia"
+    },
+    "sameAs": [
+      "https://www.facebook.com/duniaasuransi",
+      "https://www.instagram.com/duniaasuransi"
+    ]
+  }
+
+  const websiteSchema = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "name": "Dunia Asuransi",
+    "url": "https://www.duniaasuransi.com",
+    "publisher": {
+      "@type": "Organization",
+      "name": "Dunia Asuransi"
+    }
+  }
+
   return (
     <>
       <script
@@ -88,20 +40,8 @@ export default function StructuredData() {
       />
       <script
         type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessSchema) }}
-      />
-      <script
-        type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
       />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(personSchema) }}
-      />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(aggregateRatingSchema) }}
-      />
     </>
-  );
+  )
 }
